0efeecdd081be0dc7d0a8f75d6bdf8f4d4925742
zacck <coderv63@gmail.com>
2017-01-23 22:17:12 +0200

    work on redux add reducer and store prototype

---
+++ src/components/forms/MakeReservation.jsx
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+export class MakeReservation extends Component {
+  render() {
+    return(<p>Reservation form</p>)
+  }
+};
+export default MakeReservation;
---
+++ src/store/configureStore.jsx
@@ -0,0 +1,24 @@
+import * as redux from 'redux'
+import thunk from 'redux-thunk'
+
+import{reservationsReducer} from 'src/reducers/reducers'
+
+
+//lets configure our store
+export var configure = (initialState = {}) => {
+  var reducer = redux.combineReducers({
+    reservations: reservationsReducer
+  });
+
+
+  //make a redux store
+  var store = redux.createStore(reducer, initialState, redux.compose(
+    //lets add thunk to enable us to use impure actions
+    redux.applyMiddleware(thunk),
+    //do we have a dev tool if so work with it
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
+
+  //finally give back a store
+  return store;
+};
