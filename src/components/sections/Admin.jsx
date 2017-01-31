47452d67e04029c2b928de480c11678a8115f8a1
zacck <coderv63@gmail.com>
2017-01-31 14:46:18 +0200

    work on admin layout and styling

--- src/components/sections/Admin.jsx
+++ src/components/sections/Admin.jsx
@@ -1,12 +1,25 @@
 import React,{Component} from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
+
 
 export class Admin extends Component {
   constructor(props) {
     super(props);
+    this.openAddSupp = this.openAddSupp.bind(this);
+  }
+  openAddProp() {
+
+  }
+  saveProp() {
+    
   }
   render() {
     return(
-      <p>Admin Page</p>
+      <div className="row admin-panel">
+        <div className="columns small-4 large-4">
+          <RaisedButton className="raised-margin" label="Add Property" secondary={true} onClick={this.openAddProp}/>
+        </div>
+      </div>
     )
   }
 };
