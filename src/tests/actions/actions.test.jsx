44bb329c98c2cf89748de33c201b66f0123506b4
zacck <coderv63@gmail.com>
2017-02-01 11:35:34 +0200

    work on action tests and added redux-mock-store for testing

--- src/tests/actions/actions.test.jsx
+++ src/tests/actions/actions.test.jsx
@@ -1,4 +1,7 @@
 import * as actions from 'actions'
+//add ability to mock a store for testing if actions were called
+import configureMockStore from 'redux-mock-store'
+import expect from 'expect'
 
 describe('Actions', () => {
   //check that actions exist
@@ -7,5 +10,31 @@ describe('Actions', () => {
   });
 
   //work on login action test
+  it('should generate login action', () => {
+    //mock an action
+    var loginAction = {
+      type: 'LOGIN',
+      uid: 12345
+    };
+
+    //lets trigger the action
+    var result = actions.login(loginAction.uid);
+
+    //check that the result is same as mocked
+    expect(result).toEqual(loginAction);
+  });
+
   //work on logout action test
+  it('should generate logout action', () => {
+    //mock out result action
+    var logoutAction = {
+      type: 'LOGOUT'
+    };
+
+    //lets trigger
+    var result = actions.logout();
+
+    //check that the result is same as mocked
+    expect(result).toEqual(logoutAction);
+  })
 })
