b943167f43235e9072c9f7453736ead1105b943d
zacck <coderv63@gmail.com>
2017-02-01 13:07:28 +0200

    work on the login button to action login

--- src/components/sections/AuthComponent.jsx
+++ src/components/sections/AuthComponent.jsx
@@ -1,17 +1,30 @@
 import React,{Component} from 'react'
 import {hashHistory} from 'react-router'
 import * as actions from 'src/actions/actions'
+import {connect} from 'react-redux'
 
 export class AuthComponent extends Component {
   constructor(props) {
     super(props);
+    //bind login func
+    this.onLogin = this.onLogin.bind(this);
   }
-  //when component is mounted
-  componentDidMount() {
-
+  onLogin() {
+    var{dispatch} = this.props;
+    //call login action to sign in with popup
+    dispatch(actions.startLogin());
   }
   render() {
-    return(<div>Auth</div>);
+    return(
+      <div>
+         <div className="columns small-centered small-10 medium-6 large-4">
+           <div className=" text-center callout callout-auth">
+             <h3>Login</h3>
+             <button className="button" onClick={this.onLogin}> Login With Google</button>
+           </div>
+         </div>
+       </div>
+     );
   }
 };
-export default AuthComponent;
+export default connect()(AuthComponent);
