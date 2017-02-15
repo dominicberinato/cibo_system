bbe86db9fd918e8fc3c693068a23793a6e94c73f
zacck <coderv63@gmail.com>
2017-02-15 11:58:29 +0200

    work on adding firebase rules to repo and work on testing tabs component

---
+++ src/tests/components/sections/CiboTabs.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import $ from 'script-loader!jquery'
+import sinon from 'sinon'
+import * as actions from 'actions'
+import {mount} from 'enzyme'
+import expect from 'expect'
+
+import {CiboTabs} from 'CiboTabs'
+
+describe('CiboTabs', () => {
+  it('should exist',() => {
+    expect(CiboTabs).toExist();
+  });
+
+
+})
