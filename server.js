776e64d72848e56070b33a0c38adc8ad35c41fb2
zacck <coderv63@gmail.com>
2017-02-21 09:19:32 +0200

    intall compression  and enable it on the server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //load module
 var express = require('express');
+var path = require('path');
+var compression = require('compression');
 
 //create our app
 var app = express();
@@ -15,6 +17,9 @@ app.use(function (req, res, next){
   }
 });
 
+//max age of cache and enable compression
+var oneYear = 1 * 365 * 24 * 60 * 60 * 1000;
+app.use(compression());
 //show app folder to serve
 app.use(express.static(__dirname +'/dist')); // specifies a a folder name to expose
 
