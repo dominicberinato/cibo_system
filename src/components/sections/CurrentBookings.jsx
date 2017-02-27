110fb086246ff7973092dc35ba870532faf7f265
zacck <coderv63@gmail.com>
2017-02-27 16:43:47 +0200

    work on showing reservations test and code

--- src/components/sections/CurrentBookings.jsx
+++ src/components/sections/CurrentBookings.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 //TODO Create single reservation item && render list
+import Reservation from 'Reservation'
 
 export class CurrentBookings extends Component {
   constructor(props) {
@@ -14,9 +15,11 @@ export class CurrentBookings extends Component {
     var {reservations} = this.props;
     var renderReservations = () => {
       if(reservations.length != 0) {
-        return 'we have reservations!'
+        return reservations.map((res) => {
+          return(<Reservation key={res.resKey} {...res} />);
+        })
       } else {
-        return(<p ref='resservations message'>No Reservations at the moment</p>)
+        return(<p ref='eResMsg'>No Reservations at the moment</p>)
       }
     }
     return(<div className='reservation-box'>{renderReservations()}</div>)
--- src/tests/components/sections/CurrentBookings.test.jsx
+++ src/tests/components/sections/CurrentBookings.test.jsx
@@ -1,8 +1,26 @@
 import {CurrentBookings} from 'CurrentBookings'
 import expect from 'expect'
+import {shallow, mount} from 'enzyme'
+import {Reservation} from 'Reservation'
+import React from 'React'
 
 describe('<CurrentBookings/>', () => {
   it('should exist', () => {
     expect(CurrentBookings).toExist();
+  });
+
+  it('should show a reservation for each in state', () => {
+    const reservations = [{resKey: 8,name:'Isaac', tbKey: 7, time: '0800'}];
+    //shallow mount bookings and check for reservations
+    const wrapper = shallow(<CurrentBookings reservations={reservations}/>);
+    //asserrt
+    expect(wrapper.find(Reservation).length).toEqual(reservations.length);
+  });
+
+  it('should show empty reservations warnings', () => {
+    const reservations = [];
+    const wrapper = mount(<CurrentBookings reservations={reservations}/>);
+    //asserrt
+    expect(wrapper.ref('eResMsg').length).toEqual(1);
   })
 })
