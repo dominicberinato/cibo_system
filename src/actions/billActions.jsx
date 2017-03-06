04aad425942090de61eadb15e55c4cc8a60a26b5
zacck <coderv63@gmail.com>
2017-03-06 18:50:59 +0200

    work on addBill sync action

--- src/actions/billActions.jsx
+++ src/actions/billActions.jsx
@@ -1,1 +1,11 @@
 import {firebaseRef} from 'src/firebase/index'
+
+//action to add bill
+export var addBill = (bill) => {
+  return {
+    type: 'ADD_BILL',
+    bill
+  };
+};
+
+//action to add item 
