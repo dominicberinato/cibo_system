7c99481a9ec68c74b572071045a23cf5c461d663
zacck <coderv63@gmail.com>
2017-03-09 16:49:44 +0200

    fix moduleExtensions

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ var plugins = PRODUCTION
           // //purify al teh css
           new PurifyCSSPlugin({
              paths: glob.sync(path.join(__dirname, 'dist/*.html')),
-             moduleExtensions: '.html',
+             moduleExtensions: ['.html'],
              minimize: true,
              purifyOptions: { info: true, minify: true },
              verbose: true
