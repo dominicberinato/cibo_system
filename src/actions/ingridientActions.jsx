2e381a90679ae2c223b67a844cf0ed72ec00d2c2
zacck <coderv63@gmail.com>
2017-03-18 06:50:36 +0200

    work on adding async actions ingridients

--- src/actions/ingridientActions.jsx
+++ src/actions/ingridientActions.jsx
@@ -3,4 +3,19 @@ export var addIngridient = (ingridient) => {
     type: 'ADD_INGRIDIENT',
     ingridient
   };
-} 
+}
+
+export var updateIngridient = (id, updates) =>{
+  return {
+    type: 'UPDATE_INGRIDIENT',
+    id,
+    updates
+  };
+}
+
+export var deleteIngridient = (id) => {
+  return {
+    type: 'DELETE_INGRIDIENT',
+    id
+  };
+}
