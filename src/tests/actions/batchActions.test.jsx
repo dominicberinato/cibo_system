3dc5df6f510f8e2f7c531823b41902688837bd05
zacck <coderv63@gmail.com>
2017-03-28 16:14:41 +0200

    add addBatch action test

--- src/tests/actions/batchActions.test.jsx
+++ src/tests/actions/batchActions.test.jsx
@@ -8,7 +8,28 @@ describe.only('batchActions', ()=> {
 
 
   describe('sync', () => {
+    it('should create addBatch action', () => {
+      //mock an item
+      const batch = {
+        category: 'sauce',
+        description: 'Bordelause Sauce',
+        units: 'kg',
+        size: 2,
+        ingridients: [3,4],
+        cost: 3400
+      };
 
+      //mock an action
+      const batchAction = {
+        type: 'ADD_BATCH',
+        batch
+      };
+
+      const res = actions.addBatch(batch);
+
+      //assert on result
+      expect(res).toEqual(batchAction)
+    })
 
   });
 
