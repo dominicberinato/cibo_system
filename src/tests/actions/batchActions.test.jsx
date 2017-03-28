18dac252dfe9b7cb76a98df56bc7d0e6b31c44a4
zacck <coderv63@gmail.com>
2017-03-28 16:32:49 +0200

    add test for updateBatch action

--- src/tests/actions/batchActions.test.jsx
+++ src/tests/actions/batchActions.test.jsx
@@ -29,8 +29,54 @@ describe.only('batchActions', ()=> {
 
       //assert on result
       expect(res).toEqual(batchAction)
-    })
+    });
 
+    it('should create updateBatch action', () => {
+      //mock an item
+      const batch = {
+        id: 3,
+        category: 'sauce',
+        description: 'Bordelause Sauce',
+        units: 'kg',
+        size: 2,
+        ingridients: [3,4],
+        cost: 3400
+      };
+
+      //mock some updates
+      const updates  = {
+        ingredients: [3,4,5]
+      };
+
+
+      //mock a result
+      const updateBatchAction  = {
+        type:'UPDATE_BATCH',
+        id : batch.id,
+        updates
+      };
+
+      const res = actions.updateBatch(batch.id, updates);
+
+      expect(res).toEqual(updateBatchAction);
+    });
+
+    it('should create deleteBatchAction', () => {
+      //mock an item
+      const batch = {
+        id: 3,
+        category: 'sauce',
+        description: 'Bordelause Sauce',
+        units: 'kg',
+        size: 2,
+        ingridients: [3,4],
+        cost: 3400
+      };
+
+      const deleteBatchAction = {
+
+      }
+    })
   });
 
   describe('async', () => {
