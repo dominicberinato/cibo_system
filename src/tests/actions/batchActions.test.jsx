f98a38756046503ac7380a5bc6d752802a893da4
zacck <coderv63@gmail.com>
2017-03-28 16:06:49 +0200

    work on test prototype for batch actions

---
+++ src/tests/actions/batchActions.test.jsx
@@ -0,0 +1,18 @@
+import expect from 'expect'
+import * as actions from 'batchActions'
+
+describe.only('batchActions', ()=> {
+  it('should exist', () => {
+    expect(actions).toExist()
+  });
+
+
+  describe('sync', () => {
+
+
+  });
+
+  describe('async', () => {
+
+  });
+})
