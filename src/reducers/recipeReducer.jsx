166585de8bbb9c7055ef8cde5fa99537a486ac97
zacck <coderv63@gmail.com>
2017-03-31 08:55:18 +0200

    work on prototype for recipeReducer and tests

---
+++ src/reducers/recipeReducer.jsx
@@ -0,0 +1,8 @@
+export var recipeReducer  = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_RECIPE'
+      break;
+    default:
+      return state;
+  }
+}
---
+++ src/tests/reducers/recipeReducer.test.jsx
@@ -0,0 +1,80 @@
+var df = require('deep-freeze-strict');
+import expect from 'expect'
+import {recipeReducer} from 'recipeReducer'
+
+describe('recipeReducer', () => {
+  it('should exist', () => {
+    expect(recipeReducer).toExist();
+  });
+
+  it('should add recipe on addRecipe', () => {
+    //mock recipe
+    const recipe = {
+      category: 'starter',
+      description: 'portions',
+      portions: 4,
+      totalCost: 34,
+      desiredCost: 30,
+      suggestedPrice: 67,
+      id: 3
+    };
+
+    const action = {
+      type: 'ADD_RECIPE',
+      recipe
+    };
+
+    const res = recipeReducer(df([]), df(action));
+
+    expect(res[0]).toEqual(action.recipe);
+  });
+
+  it('should delete recipe on deleteRecipe', () => {
+    //mock recipe
+    const recipes = [{
+      category: 'starter',
+      description: 'portions',
+      portions: 4,
+      totalCost: 34,
+      desiredCost: 30,
+      suggestedPrice: 67,
+      id: 3
+    }];
+
+    const action = {
+      type: 'DELETE_RECIPE',
+      id: recipes[0].id
+    };
+
+    const res = recipeReducer(df(recipes), df(action));
+
+    expect(res).toExclude(recipe);
+  });
+
+  it('should update recipe on updateRecipe', () => {
+    //mock recipe
+    const recipe = [{
+      category: 'starter',
+      description: 'portions',
+      portions: 4,
+      totalCost: 34,
+      desiredCost: 30,
+      suggestedPrice: 67,
+      id: 3
+    }];
+
+    const updates = {
+      suggestedPrice: 89
+    };
+
+    const action = {
+      type: 'UPDATE_RECIPE',
+      id: recipes[0].id,
+      updates
+    };
+
+    const res = recipeReducer(df(recipes), df(action));
+
+    expect(res[0].suggestedPrice).toEqual(updates.suggestedPrice);
+  })
+})
