1dd27b213edf808f033d6f1c8a4f9c841a8d5ff2
zacck <coderv63@gmail.com>
2017-04-03 10:40:46 +0200

    work on async addmenu action

--- src/actions/menuActions.jsx
+++ src/actions/menuActions.jsx
@@ -1,3 +1,4 @@
+import {firebaseRef} from 'src/firebase'
 export var addMenu = (menu) => {
   return {
     type: 'ADD_MENU',
@@ -17,5 +18,45 @@ export var updateMenu = (id, updates) => {
     type: 'UPDATE_MENU',
     id,
     updates
+  };
+};
+
+export var startAddMenu = (menu) => {
+  return(dispatch, getState) => {
+    //get our property
+    const prop = getState().property.propKey;
+
+    //lets make a ref
+    const menuKey = firebaseRef.child('menus').push().key;
+
+    var addMenuFanOut = {};
+
+    addMenuFanOut[`/menus/${menuKey}`] = menu;
+    addMenuFanOut[`/property-menus/${prop}/${menuKey}`] = menu;
+
+    //update out fanout
+    return firebaseRef.update(addMenuFanOut).then(() => {
+      //dispatch local action
+      dispatch(addMenu({
+        id:menuKey,
+        ...menu
+      }));
+    });
+  };
+};
+
+export var startDeleteMenu = (id) => {
+  return (dispatch, getState) => {
+    //get our property
+    const prop = getState().property.propKey;
+
+    var deleteMenuFanout   =  {};
+    deleteMenuFanout[`/menus/${id}`] = null;
+    deleteMenuFanout[`/property-menus/${prop}/${id}`] = null;
+
+    //commit our update
+    return firebaseRef.update(deleteMenuFanout).then(() => {
+      dispatch(deleteMenu(id));
+    });
   };
 };
