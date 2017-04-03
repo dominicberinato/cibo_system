7f70b87e19747dce0351cf6262975bb3eb693e05
zacck <coderv63@gmail.com>
2017-04-03 11:29:06 +0200

    work on menu reducers and actions

--- src/reducers/menuReducer.jsx
+++ src/reducers/menuReducer.jsx
@@ -7,6 +7,21 @@ export var menuReducer  = (state = [], action) => {
           ...action.menu
         }
       ];
+    case 'DELETE_MENU':
+      return state.filter((menu) => {
+        return menu.id != action.id;
+      });
+    case 'UPDATE_MENU':
+      return state.map((menu) => {
+        if(menu.id == action.id) {
+          return {
+            ...menu,
+            ...action.updates
+          }
+        } else {
+          return menu;
+        }
+      });
     default:
       return state;
   }
