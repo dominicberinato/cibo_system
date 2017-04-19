fdf0d86d8541adc8d10ee0f9adace0bbd9d55043
zacck <coderv63@gmail.com>
2017-04-19 11:50:45 +0200

    work on async test to addbeverage

--- src/actions/beverageActions.jsx
+++ src/actions/beverageActions.jsx
@@ -1,7 +1,31 @@
+import {firebaseRef} from 'src/firebase/index'
+
 export var addBeverage = (beverage) => {
   return {
     type: 'ADD_BEVERAGE',
     beverage
+  };
+};
+
+
+export var startAddBeverage = (beverage) => {
+  return(dispatch, getState) => {
+    const beverageKey  = firebaseRef.child('beverages').push().key;
+    const propKey = getState().property.propKey;
+
+    //lets make a fanout
+    const beverageFanOut = {};
+
+    beverageFanOut[`/beverages/${beverageKey}`] =  beverage;
+    beverageFanOut[`/property-beverages/${beverageKey}`] = beverage;
+
+    return firebaseRef.update(beverageFanOut).then(() => {
+      //dispatch local action
+      dispatch(addBeverage({
+        id: beverageKey,
+        ...beverage
+      }));
+    });
   };
 };
 
