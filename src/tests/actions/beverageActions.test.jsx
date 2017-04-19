b8c447f5c509b41caf03bbd0aaec59d79842ee44
zacck <coderv63@gmail.com>
2017-04-19 08:01:51 +0200

    work on tests for deleting beverage

--- src/tests/actions/beverageActions.test.jsx
+++ src/tests/actions/beverageActions.test.jsx
@@ -51,8 +51,6 @@ describe.only('beverageActions', () => {
         purchasecost: 400
       };
 
-
-
       const action =  {
         type: 'UPDATE_BEVERAGE',
         id: beverage.id,
@@ -85,7 +83,142 @@ describe.only('beverageActions', () => {
 
       expect(res).toEqual(action);
     });
+  });
+
+  describe('Async', () => {
+    //do set up for async tests
+    //data needed
+    var uid;
+    var propertyRef = firebaseRef.child('properties');
+    var testPropertyKey;
+    var store;
+    var debug = {hello: "world"};
+    var mockImage = new Blob([JSON.stringify(debug, null, 2)], {type : 'application/json'});
+    var testProperty =  {
+      pname: 'folk coffee',
+      address: '3 Bree Street',
+      avatar: mockImage,
+      propKey: 58686816897
+    };
+
+    //run this code before each asnyc test (login && set up stuff)
+    beforeEach((done) => {
+      store = createMockStore({property:testProperty})
+      //SIGN IN anonymously
+      firebase.auth().signInAnonymously().then((user) => {
+        uid = user.uid;
+        testProperty.propCreator = uid
+
+        //remove any existing proprties
+        return firebaseRef.remove();
+      }).then(()=> {
+
+      })
+      .then(() => done())
+      .catch(done);
+    });
+
+    ///run this after each tests
+    afterEach((done) => {
+      firebaseRef.remove().then(() => done());
+    });
 
 
+    it('should startAddBeverage and dispatch addBeverage', (done) => {
+      const beverage =  {
+        id: 1,
+        major: 'Spirit',
+        category: 'Vodka',
+        brand: 'Grey Goose',
+        description: 'Citron',
+        unitsize: '220ml',
+        purchasesize: '6 units',
+        purchasecost: 300
+      };
+
+
+      //post to db
+      const action = actions.startAddBeverage(beverage);
+
+      //dispatch action using store
+      store.dispatch(action).then(() => {
+
+        //collect actions that were run
+        const mockActions = store.getActions();
+
+        //lets expect that add beverage had been called
+        expect(mockActions[0].toInclude({
+          type: 'ADD_BEVERAGE',
+          beverage: {
+            ...beverage
+          }
+        }));
+        done();
+      }, done())
+    });
+
+    it('should startUpdateBeverage and dispatch updateBeverage', (done) => {
+      const beverage =  {
+        id: 1,
+        major: 'Spirit',
+        category: 'Vodka',
+        brand: 'Grey Goose',
+        description: 'Citron',
+        unitsize: '220ml',
+        purchasesize: '6 units',
+        purchasecost: 300
+      };
+
+
+      const updates = {
+        purchasecost: 400
+      };
+
+      const action = actions.startUpdateBeverage(beverage.id, updates);
+
+      //lets dispatch the async actio
+      store.dispatch(action).then(() => {
+        //lets get fired actions
+        const mockActions = store.getActions();
+
+        expect(mockActions[0].toInclude({
+          type: 'UPDATE_BEVERAGE',
+          beverage: {
+            ...beverage,
+            ...updates
+          }
+        }));
+        done();
+      }, done())
+    });
+
+    it('should startDeleteBeverage and dispatch deleteBeverage', (done) => {
+      const beverage =  {
+        id: 1,
+        major: 'Spirit',
+        category: 'Vodka',
+        brand: 'Grey Goose',
+        description: 'Citron',
+        unitsize: '220ml',
+        purchasesize: '6 units',
+        purchasecost: 300
+      };
+
+      const action = actions.startDeleteBeverage(beverage.id);
+
+      store.dispatch(action).then(() => {
+        //lets collect all the fired actions
+        const mockActions = store.getActions();
+
+        //call our assertion
+        expect(mockActions[0].toInclude({
+          type: 'DELETE_BEVERAGE',
+          id: beverage.id
+        }));
+        done();
+      }, done())
+
+
+    });
   });
 })
