e6b997cb02dae196188a7e24c91586cba680f048
zacck <coderv63@gmail.com>
2017-05-19 06:39:15 +0200

    work on starting to test validation functions

---
+++ src/tests/utils/validation.test.jsx
@@ -0,0 +1,14 @@
+import * as validations from 'validation'
+import expect from 'expect'
+
+
+describe.only('Validation Rules',  () => {
+  it('should exist', () => {
+    expect(validations).toExist();
+  });
+
+  it('should return `Required` if value is undefined', () => {
+    var res =  validations.required(undefined);
+    expect(res).toEqual('Required')
+  })
+})
