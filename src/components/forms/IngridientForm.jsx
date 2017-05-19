ec09aab6d0ff8414f3031b38e2b5ae2484ed44ba
zacck <coderv63@gmail.com>
2017-05-19 07:08:40 +0200

    work on styling validation error messages work on ingridient form submission

--- src/components/forms/IngridientForm.jsx
+++ src/components/forms/IngridientForm.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {required} from 'validation'
+import {renderField} from 'renderField'
 
 export class IngridientForm extends Component {
   render() {
@@ -13,31 +14,31 @@ export class IngridientForm extends Component {
         <form ref="form" onSubmit={handleSubmit}>
         <div>
           <label>Category</label>
-          <Field name="ingridientCategory" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientCategory" type="text" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Description</label>
-          <Field name="ingridientDescription" type="text" rows="4" component="textarea" validate={[required]}/>
+          <Field name="ingridientDescription" type="text" rows="4" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Brand</label>
-          <Field name="ingridientBrand" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientBrand" type="text" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Pack Size</label>
-          <Field name="ingridientPSize" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientPSize" type="text" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Pack Cost</label>
-          <Field name="ingridientPackCost" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientPackCost" type="text" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Supplier</label>
-          <Field name="ingridientSupplier" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientSupplier" type="text" component={renderField} validate={[required]}/>
         </div>
         <div>
           <label>Estimate Life Span</label>
-          <Field name="ingridientElife" type="text" component="input" validate={[required]}/>
+          <Field name="ingridientElife" type="text" component={renderField} validate={[required]}/>
         </div>
         <div className="text-center">
           <input type="submit" value="Add Ingridient" className="button"/>
--- src/utils/renderField.js
+++ src/utils/renderField.js
@@ -1,10 +1,11 @@
+import React from 'react';
 export const renderField = ({input, label, type, meta: {touched, error, warning}}) => (
   <div>
     <label>{label}</label>
     <div>
       <input {...input} placeholder={label} type={type} />
       {touched &&
-        ((error && <span>{error}</span>) ||
+        ((error && <small className="valid-error">{error}</small>) ||
           (warning && <span>{warning}</span>))}
     </div>
   </div>
