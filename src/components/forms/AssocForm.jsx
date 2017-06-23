3ee350b581e1ed009459bc44f61f7f9258f20468
zacck <coderv63@gmail.com>
2017-06-23 05:50:28 +0200

    work on making new associationform

---
+++ src/components/forms/AssocForm.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {required} from 'validation';
+import {TextField} from 'redux-form-material-ui';
+
+export class AssocForm extends Component {
+  render() {
+    var {handleSubmit} = this.props;
+    return (
+      <div className="text-center">
+        <form ref='form' onSubmit={this.assocProduct}>
+          <label className="property-message"> ENTER PROPERTY CODE</label>
+          <Field
+            className="text-center prop-code"
+            name="propCode"
+            type="text"
+            placeholder="5GT-HTG"
+            component={TextField}
+            validate={[required]}/>
+          <input ref="submit" className="button" type="submit" value="CONNECT"/>
+        </form>
+      </div>
+    )
+  }
+}
+
+AssocForm = reduxForm({
+  form: 'assoc'
+})(AssocForm);
+
+export default AssocForm;
