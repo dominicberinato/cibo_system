c80074954bd37c1e9aa5ca89432fde02e0e33ef3
zacck <coderv63@gmail.com>
2017-08-03 07:37:15 +0200

    Work on converting IngredientForm to stateless function component

--- src/components/forms/IngridientForm.jsx
+++ src/components/forms/IngridientForm.jsx
@@ -1,95 +1,88 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Field, reduxForm} from 'redux-form'
 import {required} from 'validation'
 import {TextField, DatePicker, SelectField} from 'redux-form-material-ui'
 
 
-export class IngridientForm extends Component {
-  render() {
-    var {handleSubmit} = this.props;
-    return(
+export const IngridientForm = (handleSubmit) =>(
+    <div>
+      <p>Add Ingredient</p>
+      <form ref="form" onSubmit={handleSubmit}>
       <div>
-        <p>Add Ingredient</p>
-        <form ref="form" onSubmit={handleSubmit}>
-        <div>
-          <label>Category</label>
-          <Field
-            name="ingredientCategory"
-            type="text"
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Description</label>
-          <Field
-            name="ingredientDescription"
-            type="text"
-            multiLine={true}
-            rows={4}
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Brand</label>
-          <Field
-            name="ingredientBrand"
-            type="text"
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Pack Size</label>
-          <Field
-            name="ingredientPSize"
-            type="text"
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Pack Cost</label>
-          <Field
-            name="ingredientPackCost"
-            type="text"
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Supplier</label>
-          <Field
-            name="ingredientSupplier"
-            type="text"
-            component={TextField}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Estimate Life Span</label>
-          <Field
-            name="ingredientElife"
-            type="text"
-            component={DatePicker}
-            format={(value, name) => value === '' ? null : value}
-            validate={[required]}/>
-        </div>
-        <div>
-          <label>Allergens</label>
-          <Field
-            name="ingredientAllergen"
-            type="text"
-            placeholder="Milk, Sugar,Gluten"
-            component={TextField}
-            />
-        </div>
-        <div className="text-center">
-          <input type="submit" value="Add Ingridient" className="button"/>
-        </div>
-        </form>
+        <label>Category</label>
+        <Field
+          name="ingredientCategory"
+          type="text"
+          component={TextField}
+          validate={[required]}/>
       </div>
-    )
-  }
-};
+      <div>
+        <label>Description</label>
+        <Field
+          name="ingredientDescription"
+          type="text"
+          multiLine={true}
+          rows={4}
+          component={TextField}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Brand</label>
+        <Field
+          name="ingredientBrand"
+          type="text"
+          component={TextField}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Pack Size</label>
+        <Field
+          name="ingredientPSize"
+          type="text"
+          component={TextField}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Pack Cost</label>
+        <Field
+          name="ingredientPackCost"
+          type="text"
+          component={TextField}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Supplier</label>
+        <Field
+          name="ingredientSupplier"
+          type="text"
+          component={TextField}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Estimate Life Span</label>
+        <Field
+          name="ingredientElife"
+          type="text"
+          component={DatePicker}
+          format={(value, name) => value === '' ? null : value}
+          validate={[required]}/>
+      </div>
+      <div>
+        <label>Allergens</label>
+        <Field
+          name="ingredientAllergen"
+          type="text"
+          placeholder="Milk, Sugar,Gluten"
+          component={TextField}
+          />
+      </div>
+      <div className="text-center">
+        <input type="submit" value="Add Ingridient" className="button"/>
+      </div>
+      </form>
+    </div>
+)
 
-IngridientForm = reduxForm({
+export default reduxForm({
   form:'ingredient'
 })(IngridientForm)
-
-export default IngridientForm;
