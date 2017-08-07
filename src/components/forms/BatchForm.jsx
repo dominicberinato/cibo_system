80bbd38084a305fbbc246bafd12fe0e6a0c76581
zacck <coderv63@gmail.com>
2017-08-07 16:26:23 +0200

    Dedupe unnecessary code

--- src/components/forms/BatchForm.jsx
+++ src/components/forms/BatchForm.jsx
@@ -3,46 +3,12 @@ import {Field, reduxForm, FieldArray} from 'redux-form'
 import {required} from 'validation'
 import {renderField} from 'renderField'
 import {connect} from 'react-redux'
-import MenuItem from 'material-ui/MenuItem'
-import {SelectField} from 'redux-form-material-ui'
-
-
-
-const renderIngredients = ({fields, ingredients}) => (
-  <ul>
-    <li>
-      <button type="button" className="button" onClick={() => fields.push({})}>Add Ingredient</button>
-    </li>
-    {
-      fields.map((ingredient, index) =>
-      <li key={index}>
-        <button
-          className="button"
-          type="button"
-          title="Remove Ingredient"
-          onClick={() => fields.remove(index)}>
-          Remove Ingredient {index + 1}
-        </button>
-        <h4>Ingredient #{index + 1}</h4>
-          <Field name={`${ingredient}.name`} component={SelectField} validate={[required]} hintText="Pick An Ingredient">
-            {ingredients.map((ing) =>{
-              return(<MenuItem key={ing.id} value={ing.id} primaryText={ing.ingredientDescription}/>)
-            })}
-          </Field>
-        <Field
-          name={`${ingredient}.amount`}
-          type="text"
-          component={renderField}
-          label="Ingredient Amount"/>
-      </li>
-    )}
-  </ul>
-)
-
+import renderIngredients from 'renderIngredients'
+import renderBatches from 'renderBatches'
 
 export class BatchForm extends Component {
   render(){
-    var {handleSubmit, ingredients} = this.props;
+    var {handleSubmit, ingredients, batches} = this.props;
     return(
       <div>
         <div>
@@ -69,6 +35,9 @@ export class BatchForm extends Component {
             <FieldArray name="ingredients" ingredients={ingredients} component={renderIngredients}/>
           </div>
           <div>
+            <FieldArray name="batches" batches={batches} component={renderBatches}/>
+          </div>
+          <div>
             <label>Calculated Batch Cost</label>
             <Field name="batchCalcCost" type="text" component={renderField} disabled="disabled"/>
           </div>
@@ -88,7 +57,8 @@ BatchForm = reduxForm({
 
 BatchForm = connect((state) => {
   return{
-    ingredients: state.ingredients
+    ingredients: state.ingredients,
+    batches: state.batches
   }
 })(BatchForm)
 
