dcf4034b1a3eb86c658630c2eee24d85e3c51b84
zacck <coderv63@gmail.com>
2017-08-08 14:57:36 +0200

    Build current form for cocktail addition to the system

---
+++ src/components/forms/CocktailForm.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react'
+import {Field, reduxForm, FieldArray} from 'redux-form'
+import {TextField, DatePicker, SelectField} from 'redux-form-material-ui'
+import {required} from 'validation'
+import renderBeverage from 'renderBeverage'
+import renderIngredients from 'renderIngredients'
+export class BeverageForm extends Component {
+  render(){
+    var {handleSubmit, beverages, ingredients} = this.props;
+    return(
+      <div>
+        <p>Cocktail form</p>
+          <div>
+            <FieldArray name="cocktailBeverages" beverages={beverages} component={renderBeverage}/>
+          </div>
+          <div>
+            <FieldArray name="cocktailIngredients" ingredients={ingredients} component={renderIngredients}/>
+          </div>
+      </div>
+    )
+  }
+}
+
+CocktailForm = reduxForm({
+  form:'beverage'
+})(CocktailForm)
+CocktailForm = connect((state) => {
+  return{
+    beverages: state.beverages,
+    ingredients: state.ingredients
+  }
+})(CocktailForm)
+
+
+export default CocktailForm
