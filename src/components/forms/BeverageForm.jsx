d892e165f042c51bfb75b6d6f66ed18a53b9d552
zacck <coderv63@gmail.com>
2017-08-08 12:54:04 +0200

    Build form to add beverage details

--- src/components/forms/BeverageForm.jsx
+++ src/components/forms/BeverageForm.jsx
@@ -1,22 +1,82 @@
 import React, {Component} from 'react'
-import {Field, reduxForm} from 'redux-form'
-
+import {Field, reduxForm, FieldArray} from 'redux-form'
+import {TextField, DatePicker, SelectField} from 'redux-form-material-ui'
+import {required} from 'validation'
+import SpiritForm from 'SpiritForm'
+import WineForm from 'WineForm'
 export class BeverageForm extends Component {
   render(){
     var {handleSubmit} = this.props;
     return(
       <div>
+        <p>Add Beverage</p>
+        <form ref="form" onSubmit={handleSubmit}>
         <div>
-          <p>Add Beverage</p>
-          <form>
-            <div>
-              <label></label>
-            </div>
-            <div>
-              <input type="submit"  value="Add Beverage"/>
-            </div>
-          </form>
+          <label>Major Group</label>
+          <Field
+            name="beverageMajor"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
         </div>
+        <div>
+          <label>Description</label>
+          <Field
+            name="beverageDescription"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div>
+          <FieldArray name="beverageSpirits"  component={SpiritForm}/>
+        </div>
+        <div>
+          <FieldArray name="beverageWines"  component={WineForm}/>
+        </div>
+        <div>
+          <label>Unit</label>
+          <Field
+            name="beverageUnit"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div>
+          <label>Unit Size</label>
+          <Field
+            name="beverageUnitSize"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div>
+          <label>Purchase Unit</label>
+          <Field
+            name="beveragePurchaseUnit"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div>
+          <label>Purchase Unit Size</label>
+          <Field
+            name="beveragePurchseUnitSize"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div>
+          <label>Cost Per Purchasing Unit</label>
+          <Field
+            name="beverageCPPU"
+            type="text"
+            component={TextField}
+            validate={[required]}/>
+        </div>
+        <div className="text-center">
+          <input type="submit" value="Save Beverage" className="button"/>
+        </div>
+        </form>
       </div>
     )
   }
