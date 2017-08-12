43bf0922e37ee1ae1d0a35cd8707c790a4a498a3
zacck <coderv63@gmail.com>
2017-08-12 17:41:17 +0200

    add functionality to connect a reservation item to a  new or existing bill item

--- src/components/sections/Reservation.jsx
+++ src/components/sections/Reservation.jsx
@@ -1,15 +1,39 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import { hashHistory } from 'react-router'
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
-
-
+import { startAddBill, currBill } from 'billActions';
 
 export class Reservation extends  Component {
   constructor(props) {
-    super(props)
+    super(props);
   };
+  
+  handleClick(){
+    const { resTable, swaptabs, bills, dispatch, auth } = this.props;
+    //check if table has an existing bill 
+    const existingBill = bills.filter((bill) => {
+      return bill.tbKey == resTable
+    })
+
+    //if a bill exists for this table we should fire currBill and navigate to billing page 
+    if(existingBill.length == 1) {
+      dispatch(currBill(existingBill[0].id));
+      swaptabs('billing');
+    } else {
+      //here we don't have an existing bill we can just create a new one associated with the table and swap to billing
+      dispatch(startAddBill({
+        tbKey: resTable,
+	billCreator: auth.uid
+      }));
+      swaptabs('billing');
+
+    }
+
+        
+  }
   render() {
-    const {resOwner, resTime, resDate, resTable, tables} = this.props;
+    const {resOwner, resTime, resDate, resTable, tables, auth, swaptabs} = this.props;
     var renderRes = () => {
       const table = tables.find((table) => {
         return table.tbKey == resTable;
@@ -18,7 +42,7 @@ export class Reservation extends  Component {
         return(<p ref="reservationError"> Reloading reservation</p>)
       } else {
         return(
-          <Card className='res-item'>
+          <Card onClick={()=>{this.handleClick()}} className='res-item'>
             <CardHeader
               title={`${resOwner} `}
             />
@@ -43,6 +67,8 @@ export class Reservation extends  Component {
 
 export default connect((state) => {
   return {
-    tables: state.tables
+    tables: state.tables,
+    bills: state.bills,
+    auth: state.auth
   }
 })(Reservation);
