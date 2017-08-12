75fccbeebb53a3df078393654913f8a31019c324
zacck <coderv63@gmail.com>
2017-08-12 07:22:00 +0200

    Add price field to an item in the bill object this  add UI elements to display bill item name and price, Apply Prettier to file. Fixes #88

--- src/components/sections/CurrentBill.jsx
+++ src/components/sections/CurrentBill.jsx
@@ -1,61 +1,70 @@
-import React, {Component, PropTypes} from 'react'
-import {connect} from 'react-redux'
-import {addItem} from 'billActions'
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import { addItem } from "billActions";
 
 export class CurrentBill extends Component {
   constructor(props) {
     super(props);
     this.addItem = this.addItem.bind(this);
-    this.clearBill  = this.clearBill.bind(this);
-  };
-  addItem(e){
+    this.clearBill = this.clearBill.bind(this);
+  }
+  addItem(e) {
     e.preventDefault();
     //lets get props from connect
-    var {dispatch, currBill} = this.props;
+    var { dispatch, currBill } = this.props;
 
     //lets validate
     const itemID = this.refs.orderID.value;
 
-    if(itemID.length > 0 ) {
+    if (itemID.length > 0) {
       //dispatch updates
       const updates = {
-        items:[
-          itemID
-        ]
+	      items: [{name:itemID, price:32}]
       };
 
       //call dispatch with  our orders
       dispatch(addItem(currBill, updates));
     }
-
   }
 
   clearBill(e) {
     e.preventDefault();
   }
   render() {
-    var {currBill, bills} = this.props;
-    var currBillItems = () => {
-      if(currBill === '') {
-        return(<p className="no-bill text-center"> Please Select a Bill for Details</p>)
+    const { currBill, bills } = this.props;
+    const currBillItems = () => {
+      if (currBill === "") {
+        return (
+          <p className="no-bill text-center">
+	   Please Select a Bill for Details
+          </p>
+        );
       }
       //pick current bill from state
-      var myBill = bills.find((billItem) => {
-        return billItem.id == currBill
-      })
+      const myBill = bills.find(billItem => {
+        return billItem.id == currBill;
+      });
       //check items
-      if(myBill.items != undefined) {
+      if (myBill.items != undefined) {
         //list items
-        return(
+        return (
           <div>
-            <div className="item-orders text-center">{myBill.items.map((item) => {
-                return(<p className="item-line">{item}</p>)
-              })}</div>
+            <div className="item-orders text-center">
+              {myBill.items.map(item => {
+		const time = Date.now();
+                return (
+                  <div key={ item.name+time } className="item-line">
+                    <div className="small-6 item-name"> <p> { item.name } </p> </div>
+		    <div className="small-6 item-price"> <p> { item.price } </p> </div>
+                  </div>
+                );
+              })}
+            </div>
             <div className="text-center ">
-              <form ref='form' onSubmit={this.addItem}>
+              <form ref="form" onSubmit={this.addItem}>
                 <div>
                   <label>Order Item</label>
-                  <input className='item-input' type='text' ref='orderID'/>
+                  <input className="item-input" type="text" ref="orderID" />
                 </div>
                 <div>
                   <button className="button ">Add Item</button>
@@ -66,16 +75,17 @@ export class CurrentBill extends Component {
               <button className="button alert">Clear</button>
             </div>
           </div>
-        )
-      } else {
-        return(
+        );
+      } 
+      else {
+        return (
           <div>
             <p className="no-orders text-center"> Please take orders</p>
             <div className="text-center ">
-              <form ref='form' onSubmit={this.addItem}>
+              <form ref="form" onSubmit={this.addItem}>
                 <div>
                   <label>Order Item</label>
-                  <input className='item-input' type='text' ref='orderID'/>
+                  <input className="item-input" type="text" ref="orderID" />
                 </div>
                 <div>
                   <button className="button ">Add Item</button>
@@ -86,36 +96,36 @@ export class CurrentBill extends Component {
               <button className="button alert">Clear</button>
             </div>
           </div>
-        )
+        );
       }
-    }
-    return(
+    };
+    return (
       <div className="current-bill">
         <div className="text-center">
-           <p>Items</p>
+          <p>Items</p>
         </div>
         <div>
           {currBillItems()}
         </div>
       </div>
-    )
+    );
   }
-};
+}
 
 //define expected props
-CurrentBill.propTypes  = {
+CurrentBill.propTypes = {
   currBill: PropTypes.string,
   bills: PropTypes.array
 };
 
 //define default props
 CurrentBill.defaultProps = {
-  currBill: '',
+  currBill: "",
   bills: []
-}
-export default connect((state) => {
+};
+export default connect(state => {
   return {
     currBill: state.currBill,
     bills: state.bills
-  }
+  };
 })(CurrentBill);
