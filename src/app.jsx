627c17ae64533ec13a7dc703f55005218dd29ccc
zacck <coderv63@gmail.com>
2017-08-12 05:40:15 +0200

    work on spacing of file imports

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 
- import React from 'react'
- import ReactDOM from 'react-dom'
- import {Provider} from 'react-redux'
- import {hashHistory} from 'react-router'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {hashHistory} from 'react-router'
 
 
 // load jquery and foundation in the window scope
